4b4f9ffec5a82c9d962ae5570c79a910acefda97
Maks Verver <maks@verver.ch>
2024-02-05 02:19:59 +0100

    Add Python implementations of `inspect` and `search`.
    Just to demonstrate that with the Python bindings, it's easy to implement
    tools like this in Python rather than C++.

---
+++ python/inspect.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# A Python script that implements the same functionality as the C++ inspect app.
+
+import sys
+import wikipath
+
+if len(sys.argv) != 3:
+  print(f'Usage: {sys.argv[0]} <wiki.graph> <PageTitle|#PageId|?>')
+  sys.exit(1)
+
+_, graph_filename, page_arg = sys.argv
+
+reader = wikipath.Reader(graph_filename)
+page_id = reader.parse_page_argument(page_arg)
+if not page_id:
+  print('Page not found!')
+  sys.exit(1)
+
+print(reader.page_ref(page_id))
+
+print("Outgoing links:")
+for destination in reader.graph.forward_edges(page_id):
+  print(' -> ' + reader.forward_link_ref(page_id, destination))
+
+print("Incoming links:")
+for source in reader.graph.backward_edges(page_id):
+  print(' <- ' + reader.backward_link_ref(source, page_id))
